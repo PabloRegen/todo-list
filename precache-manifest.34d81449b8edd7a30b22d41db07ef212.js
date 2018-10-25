f5b5f19597e7a849a66ab28f68fb6d902d6da4ce
Pablo Regen <pabloregen@users.noreply.github.com>
2018-10-25 17:57:07 -0400

    Updates

---
+++ precache-manifest.34d81449b8edd7a30b22d41db07ef212.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "6041c9494691b9da7169",
+    "url": "/todo-list/static/js/main.6041c949.chunk.js"
+  },
+  {
+    "revision": "e139b463c43dbe452b3b",
+    "url": "/todo-list/static/js/1.e139b463.chunk.js"
+  },
+  {
+    "revision": "6041c9494691b9da7169",
+    "url": "/todo-list/static/css/main.996d72a4.chunk.css"
+  },
+  {
+    "revision": "ab31b5024002e50a28e7000f13b180d9",
+    "url": "/todo-list/index.html"
+  }
+];
