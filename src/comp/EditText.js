fa3baa0b88c4195c3b712ce9de10246879e48907
Pablo Regen <pabloregen@users.noreply.github.com>
2018-11-01 21:23:58 -0400

    Improve style

--- src/comp/EditText.js
+++ src/comp/EditText.js
@@ -3,10 +3,12 @@ import React from 'react';
 const EditText = ({ text, id, onTaskEdit, onSaveEditedTask }) => (
     <span>
         <input
+            className='editText'
             type='text'
             value={text}
             onChange={e => onTaskEdit(e, id)} />
         <button
+            className='saveBtn'
             type='button'
             onClick={e => onSaveEditedTask(e, id)}>
             Save
